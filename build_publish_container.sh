80b7cc15892e8c01673862418bbf88af5a304bae
Bart Wijnants <bartwijnants@outlook.com>
2017-12-02 09:23:38 +0100

    fix travis and heroku

--- build_publish_container.sh
+++ build_publish_container.sh
@@ -4,4 +4,4 @@ docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $image
 docker login -u=$HEROKU_USERNAME -p=$HEROKU_AUTH_TOKEN registry.heroku.com;
 docker tag $image registry.heroku.com/dapeton/web;
-docker push registry.heroku.com/notepad/web;
+docker push registry.heroku.com/dapeton/web;
